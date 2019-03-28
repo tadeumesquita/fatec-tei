f52a1bc907cd51c2067801fa89c255024a7a1793
Tadeu Mesquita <tadeumesquita@gmail.com>
2019-03-28 20:44:13 -0300

    O exercicio 6 foi finalizado

--- tei-a04/tei-a04-exercicio-06.py
+++ tei-a04/tei-a04-exercicio-06.py
@@ -16,7 +16,6 @@ for veiculo in data.json():
     print("Ano e Combustivel:",veiculo['name'],"- ID:", veiculo['id'])
 
 idAnoCombustivel = input("Digite o ID da Ano/Combustível: ")
-#data = requests.get(url="http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(idModelo)+"/"+str(idAnoCombustivel)+".json")
-data = requests.get(url="http://fipeapi.appspot.com/api/1/carros/veiculo/21/4828/2013-1.json")
-for veiculo in data.json():
-    print(veiculo)
+data = requests.get(url="http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(idModelo)+"/"+str(idAnoCombustivel)+".json")
+
+print("Valor do carro: ",data.json()['preco'])

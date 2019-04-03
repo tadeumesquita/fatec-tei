db692983e9712e91cde06294354f5d3ba0b5c827
Tadeu Mesquita <tadeumesquita@gmail.com>
2019-04-02 23:35:27 -0300

    Exercicio 2 da aula 2

--- tei-a02/tei-a02-exercicio-02.py
+++ tei-a02/tei-a02-exercicio-02.py
@@ -1,20 +1,12 @@
-import os
-os.system('cls')
+resistencias = []
 
-nome = input("Digite o nome do produto: ")
-valor = float(input("Digite o valor do produto: "))
+for i in range(4):
+    resistencias.append(int(input("Digite o valor da resistencia: ")))
 
-if valor >=0 and valor < 50:
-    res = valor
-elif valor >=50 and valor < 200:
-    res = valor*0.95
-elif valor >= 200 and valor < 500:
-    res = valor*0.94
-elif valor >= 500 and valor < 1000:
-    res = valor*0.93
-elif valor >= 1000:
-    res = valor*0.92
-else:
-    print("Produto com valor negativo!")
+print("Resistencias fornecidas:")
+for resistencia in resistencias:
+    print(resistencia,end=", ")
 
-print("Produto: {0}\nValor Original: {1}\nValor com Desconto: {2}".format(nome,round(valor,2),round(res,2)))
+print("\nA resistencia equivalente e:",sum(resistencias))
+print("A maior resistencia e:",max(resistencias))
+print("A menor resistencia e:",min(resistencias))

57cb5533ba4e7ba9bbd5787671393f6781c4f744
Tadeu Mesquita <tadeumesquita@gmail.com>
2019-04-03 00:41:31 -0300

    Exercicio 3 da aula 3

--- tei-a03/tei-a03-exercicio-03.py
+++ tei-a03/tei-a03-exercicio-03.py
@@ -1,28 +1,35 @@
-def isPar(valor):
-    if valor % 2 == 0:
-        return True
-    else:
-        return False
+import collections
 
-def qtdPares(valores):
-    count = 0
-    for valor in valores:
-        if isPar(valor):
-            count+=1
-    return count
+Aluno = collections.namedtuple("Aluno","nome nota1 nota2 media situacao")
 
-def qtdImpares(valores):
-    return len(valores)-qtdPares(valores)
+alunos = []
+somaMedia, aprovados, reprovados = 0, 0, 0
 
-def main():
-    numeros = (1,2,3,4,5,6)
+#alunos.append(Aluno("João da Silva", 5, 6, (5+6/2), "Aprovado" if (5+6/2) >= 5 else "Reprovado"))
 
-    print(numeros)
-    print("Quantidade de pares:", qtdPares(numeros))
-    print("Quantidade de impares:", qtdImpares(numeros))
-    print("Maior valor:", max(numeros))
-    print("Menor valor:", min(numeros))
-    print("Soma dos valores:", sum(numeros))
-    print("Quantidade de itens:", len(numeros))
-    
-main()
+for i in range(2):
+    nome = input("Digite o nome: ")
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    media = (nota1+nota2)/2
+    alunos.append(Aluno(nome,nota1,nota2,media, "Aprovado" if media >= 5 else "Reprovado"))
+
+print("Relatório das Notas")
+print("Aluno  Nota1  Nota2  Media  Situacao")
+for aluno in alunos:
+    print("%s" %aluno.nome,end="  ")
+    print("%f" %aluno.nota1,end="  ")
+    print("%f" %aluno.nota2,end="  ")
+    print("%f" %aluno.media,end="  ")
+    print("%s" %aluno.situacao,end="  ")
+    print("")
+
+    somaMedia = somaMedia + aluno.media
+    if aluno.situacao == "Aprovado":
+        aprovados = aprovados + 1 
+    if aluno.situacao == "Reprovado":
+        reprovados = reprovados + 1 
+
+print("Media da sala:",somaMedia/len(alunos))
+print("Total de aprovados:",aprovados)
+print("Total de reprovados:",reprovados)

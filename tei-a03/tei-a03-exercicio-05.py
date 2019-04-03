4e635e3f2cd5314687e6ae10d825f79ebd4a6d27
Tadeu Mesquita <tadeumesquita@gmail.com>
2019-04-03 01:16:53 -0300

    Exercicio 5 da aula 3

--- tei-a03/tei-a03-exercicio-05.py
+++ tei-a03/tei-a03-exercicio-05.py
@@ -1,28 +1,14 @@
-def isPar(valor):
-    if valor % 2 == 0:
+def checkPalindrome(lista):
+    listaAoContrario = lista.copy()
+    listaAoContrario.reverse()
+    #print(cmp(lista,listaAoContrario))
+    
+    if lista == listaAoContrario:
         return True
     else:
         return False
-
-def qtdPares(valores):
-    count = 0
-    for valor in valores:
-        if isPar(valor):
-            count+=1
-    return count
-
-def qtdImpares(valores):
-    return len(valores)-qtdPares(valores)
-
-def main():
-    numeros = (1,2,3,4,5,6)
-
-    print(numeros)
-    print("Quantidade de pares:", qtdPares(numeros))
-    print("Quantidade de impares:", qtdImpares(numeros))
-    print("Maior valor:", max(numeros))
-    print("Menor valor:", min(numeros))
-    print("Soma dos valores:", sum(numeros))
-    print("Quantidade de itens:", len(numeros))
     
+def main():
+    print(checkPalindrome(list(input("Digite uma palavra ou frase para saber se e um palindromo: "))))
+        
 main()

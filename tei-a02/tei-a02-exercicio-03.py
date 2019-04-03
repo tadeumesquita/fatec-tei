92b4605b28eeefc25b5f93e276b89c20d2400158
Tadeu Mesquita <tadeumesquita@gmail.com>
2019-04-02 23:41:51 -0300

    Exercicio 3 da aula 2

--- tei-a02/tei-a02-exercicio-03.py
+++ tei-a02/tei-a02-exercicio-03.py
@@ -1,20 +1,6 @@
-import os
-os.system('cls')
+numero = int(input("Digite um numero: "))
 
-nome = input("Digite o nome do produto: ")
-valor = float(input("Digite o valor do produto: "))
+print("Tabuada do",numero)
 
-if valor >=0 and valor < 50:
-    res = valor
-elif valor >=50 and valor < 200:
-    res = valor*0.95
-elif valor >= 200 and valor < 500:
-    res = valor*0.94
-elif valor >= 500 and valor < 1000:
-    res = valor*0.93
-elif valor >= 1000:
-    res = valor*0.92
-else:
-    print("Produto com valor negativo!")
-
-print("Produto: {0}\nValor Original: {1}\nValor com Desconto: {2}".format(nome,round(valor,2),round(res,2)))
+for i in range(1,11):
+    print("{0} x {1} = {2}".format(numero,i,(numero*i)))
